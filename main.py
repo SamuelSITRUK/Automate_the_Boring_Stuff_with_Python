c82cebbecba60a3f49be1a45819d6906a04f7277
Samuel Sitruk <samuel.sitruk.galbauddufort@gmail.com>
2025-02-26 12:43:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -138,3 +138,13 @@ w [a-zA-Z0-9.-]+ # domain name
 '''
 
 
+#3/ Find All Matches in the Clipboard Text
+text = str(pyperclip.paste())
+matches = []
+for groups in phoneRegex.findall(text):
+  phoneNum = '-'.join([groups[1], groups[3], groups[5]])
+  if groups[8] != '':
+    phoneNum += ' x' + groups[8]
+  matches.append(phoneNum)
+for groups in emailRegex.findall(text):
+  matches.append(groups[0])
